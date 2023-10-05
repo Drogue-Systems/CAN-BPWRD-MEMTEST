01c2f9a9740c5eedbd51abfa2a9c91408f097c0f
Jack <33493680+jnoxro@users.noreply.github.com>
2023-10-05 22:09:04 +0100

    initial commit

---
+++ CAN-BPWRD-MEMTEST/src/N24C32.cpp
@@ -0,0 +1,119 @@
+/*
+File Name:  N24C32.cpp
+Author:     Jack Orton
+
+
+Description:
+
+A I2C wrapper allowing use of the N24C32 EEPROM in arduino framework using the Wire library
+*/
+
+#include "N24C32.h"
+TwoWire Wire2(PB7, PA15); //TODO remove this from here ideally
+
+//default A2, A1 and A0 to 0, giving an address of 0x50
+int N24C32_address = 0x50;
+
+void N24C32_wire_config(uint32_t scl, uint32_t sda, uint32_t freq_khz)
+{
+    //configure I2C for memory interface
+    Wire2.flush();
+    Wire2.setSCL(scl);
+    Wire2.setSDA(sda);
+    Wire2.begin();
+    Wire2.setClock(freq_khz*1000);
+}
+
+// void N24C32_wire_config(uint32_t scl, uint32_t sda, uint32_t freq_khz, int hex_address)
+// {
+//     //configure I2C for memory interface
+//     Wire2.setSCL(scl);
+//     Wire2.setSDA(sda);
+//     //Wire2.setClock(freq_khz*1000);
+//     Wire2.begin();
+
+//     N24C32_address = hex_address;
+// }
+
+
+byte N24C32_convert_U16_high_order(uint16_t u16_val)
+{
+    return u16_val & 0xff;
+}
+byte N24C32_convert_U16_low_order(uint16_t u16_val)
+{
+    return (u16_val >> 8) & 0xff;
+}
+
+int N24C32_byte_write(byte address_1, byte address_2, byte data)
+{
+    Wire2.beginTransmission(N24C32_address);
+    Wire2.write(address_1);
+    Wire2.write(address_2);
+    Wire2.write(data);
+   // Serial2.print("end send: ");
+  //  Serial2.println(Wire2.endTransmission());
+
+    return 1;
+
+}
+
+int N24C32_page_write(byte address_1, byte address_2, byte *data, int num_bytes)
+{
+    if (num_bytes > 32)
+    {
+        return 0;
+    }
+
+    Wire2.beginTransmission(N24C32_address);
+    Wire2.write(address_1);
+    Wire2.write(address_2);
+    for (int i = 0; i < num_bytes; i++)
+    {
+        Wire2.write(data[i]);
+    }
+    
+    Wire2.endTransmission();
+
+    return 1;
+}
+
+int N24C32_immediate_read(byte *data, uint16_t num_bytes)
+{
+    int num_bytes_received = Wire2.requestFrom(N24C32_address, num_bytes);
+
+    if (num_bytes_received > num_bytes)
+    {
+        return 0;
+    }
+
+    for (int i = 0; i < num_bytes_received; i++)
+    {
+        data[i] = Wire2.read();
+    }
+
+    return 1;
+}
+
+int N24C32_selective_read(byte address_1, byte address_2, byte *data, uint16_t num_bytes)
+{
+    Wire2.beginTransmission(N24C32_address);
+    Wire2.write(address_1);
+    Wire2.write(address_2);
+    Wire2.endTransmission();
+
+    int num_bytes_received = Wire2.requestFrom(N24C32_address, num_bytes);
+    // Serial2.print("     bytes received: ");
+    // Serial2.print(num_bytes_received);
+    if (num_bytes_received > num_bytes)
+    {
+        return 0;
+    }
+
+    for (int i = 0; i < num_bytes_received; i++)
+    {
+        data[i] = Wire2.read();
+    }
+
+    return 1;
+}
---
+++ CAN-BPWRD-MEMTEST/src/main.cpp
@@ -0,0 +1,56 @@
+#include <Arduino.h>
+#include "N24C32.h"
+#include <Wire.h>
+
+
+
+void setup() {
+
+  N24C32_wire_config(PA15, PB7, 400);
+
+
+  Serial2.begin(115200);
+  Serial2.println("start");
+
+
+
+  delay(100);
+
+}
+
+void loop() {
+
+
+  Serial2.println("write 0xAA to address 0x02 0x02");
+  N24C32_byte_write(0x02, 0x02, 0xAA);
+  delay(500);
+  Serial2.println("write 0xBB to address 0x02 0x03");
+  N24C32_byte_write(0x02, 0x03, 0xBB);
+  delay(500);
+  Serial2.println("write 0xCC to address 0x13 0x09");
+  N24C32_byte_write(0x13, 0x09, 0xCC);
+
+
+  byte mydata = 0x00;
+  delay(1000);
+  Serial2.print("read 0x02 0x02: ");
+  N24C32_selective_read(0x02, 0x02, &mydata, 1);
+  Serial2.println(mydata, HEX);
+
+  mydata = 0x00;
+  delay(500);
+  Serial2.print("read 0x02 0x03: ");
+  N24C32_selective_read(0x02, 0x03, &mydata, 1);
+  Serial2.println(mydata, HEX);
+
+  mydata = 0x00;
+  delay(500);
+  Serial2.print("read 0x13 0x09: ");
+  N24C32_selective_read(0x13, 0x09, &mydata, 1);
+  Serial2.println(mydata, HEX);
+
+
+  delay(5000);
+
+}
+
